4f5977d5b5fafe5e100c044583512a0df659cbec
alisoncruz <alisonn83@gmail.com>
2022-08-11 17:46:17 -0300

    Mapeamento das Classes de Domínio

--- src/main/java/br/com/academiadigital/entity/Matricula.java
+++ src/main/java/br/com/academiadigital/entity/Matricula.java
@@ -1,11 +1,25 @@
 package br.com.academiadigital.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_matriculas")
 public class Matricula {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_aluno")
     private Aluno aluno;
 
     private LocalDateTime data = LocalDateTime.now();
