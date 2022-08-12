abe81b712aa8f876ac9735eb5ee77a7c763a1b4f
alisoncruz <alisonn83@gmail.com>
2022-08-12 15:00:29 -0300

    Adicionado mapeamento Lombok nas classes Form

--- src/main/java/br/com/academiadigital/entity/form/AlunoForm.java
+++ src/main/java/br/com/academiadigital/entity/form/AlunoForm.java
@@ -1,7 +1,14 @@
 package br.com.academiadigital.entity.form;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AlunoForm {
     private String nome;
     private String cpf;
