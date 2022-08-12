df5574a06c1ca7032923d301f89853f92008d358
alisoncruz <alisonn83@gmail.com>
2022-08-12 17:32:01 -0300

    Adicionado pacote infra

---
+++ src/main/java/br/com/academiadigital/infra/jackson/ObjectMapperConfig.java
@@ -0,0 +1,27 @@
+package br.com.academiadigital.infra.jackson;
+
+
+import br.com.academiadigital.infra.jackson.deserializer.LocalDateDeserializer;
+import br.com.academiadigital.infra.jackson.deserializer.LocalDateTimeDeserializer;
+import br.com.academiadigital.infra.jackson.serializer.LocalDateSerializer;
+import br.com.academiadigital.infra.jackson.serializer.LocalDateTimeSerializer;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Configuration
+public class ObjectMapperConfig implements Jackson2ObjectMapperBuilderCustomizer {
+
+    @Override
+    public void customize(Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder) {
+        jacksonObjectMapperBuilder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .serializerByType(LocalDate.class, new LocalDateSerializer())
+                .serializerByType(LocalDateTime.class, new LocalDateTimeSerializer())
+                .deserializerByType(LocalDate.class, new LocalDateDeserializer())
+                .deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer());
+    }
+}
---
+++ src/main/java/br/com/academiadigital/infra/jackson/serializer/LocalDateTimeSerializer.java
@@ -0,0 +1,23 @@
+package br.com.academiadigital.infra.jackson.serializer;
+
+import br.com.academiadigital.infra.utils.JavaTimeUtils;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class LocalDateTimeSerializer extends StdSerializer<LocalDateTime> {
+    private static final long serialVersionUID = -2718386750062666481L;
+
+    public LocalDateTimeSerializer() {
+        super(LocalDateTime.class);
+    }
+
+    @Override
+    public void serialize(final LocalDateTime value, final JsonGenerator generator, final SerializerProvider provider)
+            throws IOException {
+        generator.writeString(value.format(JavaTimeUtils.LOCAL_DATE_TIME_FORMATTER));
+    }
+}
